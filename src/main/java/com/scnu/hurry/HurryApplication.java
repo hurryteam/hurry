8ba56b22b50211a9b1106c5151410f569754891d
ccbhj <iamcbj@163.com>
2019-07-09 20:13:24 +0800

    start idea project

---
+++ src/main/java/com/scnu/hurry/HurryApplication.java
@@ -0,0 +1,13 @@
+package com.scnu.hurry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HurryApplication.class, args);
+    }
+
+}
