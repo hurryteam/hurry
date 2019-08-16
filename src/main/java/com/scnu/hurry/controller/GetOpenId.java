3faf323556f8c8a511eb8e72667991a89d438860
ccbhj <iamcbj@163.com>
2019-08-16 17:50:00 +0800

    add getopenid controller

--- src/main/java/com/scnu/hurry/controller/GetOpenId.java
+++ src/main/java/com/scnu/hurry/controller/GetOpenId.java
@@ -2,16 +2,28 @@ package com.scnu.hurry.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.scnu.hurry.util.HttpRequest;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value = "用户openid")
 public class GetOpenId {
 
     //appid
     //secret
     //code
+    @ApiOperation(value = "返回用户Openid")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "appid", value = "小程序appid", dataType = "string", paramType = "query", required = true),
+            @ApiImplicitParam(name = "secret", value = "小程序密钥", dataType = "string", paramType = "query", required = true),
+            @ApiImplicitParam(name = "code", value = "登录凭证", dataType = "string", paramType = "query", required = true),
+    })
     @GetMapping("getOpenid")
     public String getOpenid(@RequestParam("appid") String appid,
                          @RequestParam("secret") String secret,
